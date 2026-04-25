d899a7947d6ac4d9b85c75ad69205602861d011b
ayushmaan-da <ayushmaandaksh@gmail.com>
2026-04-25 19:32:33 +0530

    fix files

--- server.js
+++ server.js
@@ -1,12 +1,25 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 
 const app = express();
+
+app.use(cors());
+
+// load JSON
+const ParkSimulator = require("./ParkingSlot.json");
+
+// routes
+app.get("/", (req, res) => {
+  res.send("API working 🚀");
+});
+
+app.get("/slots", (req, res) => {
+  res.json(data);
+});
+
+// IMPORTANT for Render
 const PORT = process.env.PORT || 5000;
 
-// Middleware
-app.use(cors()); // Enable CORS for all routes
-app.use(express.json()); // Parse JSON request bodies
-
-const ParkSimulator = require("./ParkingSlot.json")
-
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
